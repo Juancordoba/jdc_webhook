17af5385e6270897291bf536fbd8d74ace9b3f2b
juanocordoba <juancordoba75@gmail.com>
2023-12-29 23:19:40 -0300

    error resp 3

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -1,17 +1,38 @@
-import { NextRequest, NextResponse } from 'next/server';
- 
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { NextRequest } from 'next/server';
+
 const WHATSAPP_TOKEN = process.env.WHATSAPP_TOKEN
 const VERIFY_TOKEN = process.env.VERIFY_TOKEN
 
-export async function GET(req: NextRequest, res: NextResponse) {
+export async function GET(req: NextRequest) {
 
-    let resp:any = {}
   const mode = req.nextUrl.searchParams.get("hub.mode");
   let token = req.nextUrl.searchParams.get("hub.verify_token");
   let challenge: string = req.nextUrl.searchParams.get("hub.challenge") as string;
 
-  // Check if a token and mode were sent
   if (mode && token) {
+    if (mode === "subscribe" && token === VERIFY_TOKEN) {
+      return Response.json(challenge);
+    } else {
+      // Responds with '403 Forbidden' if verify tokens do not match
+      return Response.json({ error: 'Forbidden Error'} , {status: 403 });
+    }
+  }
+
+}
+
+/*
+
+export async function GET(  
+                req: NextApiRequest,
+                res: NextApiResponse<ResponseData>) {
+
+/*
+  let resp:any = {}
+
+
+  // Check if a token and mode were sent
+
 
     // Check the mode and token sent are correct
     if (mode === "subscribe" && token === VERIFY_TOKEN) {
@@ -28,9 +49,12 @@ export async function GET(req: NextRequest, res: NextResponse) {
   }
 
 
+res.status(200).json({ message: 'Hello from Next.js!' })
+
 
 }
-
+*/
+/*
 export async function POST( req: NextRequest, res: NextResponse) {
 
     const body = await req.json()
@@ -38,3 +62,5 @@ export async function POST( req: NextRequest, res: NextResponse) {
 
     return NextResponse.json({ method: 'POST' });
   }
+
+  */
